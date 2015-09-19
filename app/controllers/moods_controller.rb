08a909c1060e8b00974d6c9dd93ffcf03a76c662
Joshua Swiss <swissjoshua@gmail.com>
2015-09-19 12:12:21 +0100

    basic layout structured

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,2 +1,7 @@
 class MoodsController < ApplicationController
+
+	def index
+		@moods = Mood.all
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :movies
   resources :comments
 
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
+  get '/search' => 'pages#search'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
