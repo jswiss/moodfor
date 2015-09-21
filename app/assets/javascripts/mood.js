80b1c6becd685bcf440c22e03058d4f477e03fa2
Joshua Swiss <swissjoshua@gmail.com>
2015-09-21 14:12:30 +0100

    pulling data from db and sending get to api

--- app/assets/javascripts/mood.js
+++ app/assets/javascripts/mood.js
@@ -1,36 +1,34 @@
 $(document).ready(function() {
 
-	var moodId = $('.mood-info').data('mood-id');
-	// var mood = $('.mood-name');
+	var moodId = $('.mood-title').data(mood.id);
+	var mood = $('.mood-link');
 	var moodResults = $('#mood-results');
 	console.log('I AM MOODY')
 
-	console.log(moodId);
+	$('.mood-card').on('click', genreSearch);
 
-	$('.mood-card').on('click', search);
-
-	function search(e) {
-		console.log('hi');
+	function genreSearch(e) {
+		console.log('moodId', moodId);
 		e.preventDefault();
-		var genreId = mood.data('db_genre_id');
-		console.log(genreId)
+		var genreId = e.target.getAttribute('data');
+		console.log('genreId', genreId)
 		$.get('https://api.themoviedb.org/3/discover/movie?api_key=8a40a9b70991be8016678a3ffc4d6aa2&with_genres=' + genreId, function(response) {
-			console.log(response)
+			console.log('response', response)
 			render(response);
 		});
 	}
 
-	// function render(response) {
-	// 	console.log(response);
-	// 	var data = response.results
-	// 	$.each(data, function(index, value) {
-	// 		var movieCardTemplate = '<div class ="movie-card">';
-	// 		movieCardTemplate += '<img src="' + value.poster_path + '" alt="">';
-	// 		movieCardTemplate += '<h2 class="card-title">"' + value.original_title + '"</h2>';
-	// 		movieCardTemplate += '<p class="card-overview">"' + value.original_title + '"</p>';
-	// 		movieCardTemplate += '</div>'
-	// 	})
-	// 	results.hide().append(trackCardTemplate).fadeIn('slow');
+	function render(response) {
+		console.log(response);
+		var data = response
+		$.each(data, function(index, value) {
+			var movieCardTemplate = '<div class ="movie-card">';
+			movieCardTemplate += '<img src="' + value.poster_path + '" alt="">';
+			movieCardTemplate += '<h2 class="card-title">"' + value.original_title + '"</h2>';
+			movieCardTemplate += '<p class="card-overview">"' + value.original_title + '"</p>';
+			movieCardTemplate += '</div>'
+		})
+		// moodResults.hide().append(movieCardTemplate).fadeIn('slow');
 
-	// }
+	}
 });
